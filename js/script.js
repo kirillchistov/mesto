f620370d1d5b7c01147285b68c86c7d55a205541
Kirill Chistov <kirill.v.chistov@yandex.ru>
2022-05-21 12:21:08 +0300

    PR4 release 1.1 desperate version full of bugs

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// Находим в DOM попап и его элементы, присваиваем константам
+
+let popup = document.querySelector('.popup');
+let btnEditProfile = document.querySelector('.profile__button-edit');
+let btnClosePopup = document.querySelector('.popup__close');
+let profileEditForm = document.forms.profileEditForm;
+/* let profileEditForm = document.forms['profileEdit']; */
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+let profileNameInput = document.querySelector('.popup__input-name');
+let profileJobInput = document.querySelector('.popup__input-job');
+
+// добавляем модификатор, чтобы попап открывался 
+// Подставляем в поля текущие значения со страницы, убирая пробелы 
+
+function popupOpen() {
+    console.log("here we go");
+    popup.classList.add('popup_opened');
+    profileNameInput.value = profileName.textContent.trim();
+    profileJobInput.value = profileJob.textContent.trim();
+}
+
+// чтобы закрыть попап, удаляем модификатор 
+
+function popupClose() {
+    popup.classList.remove('popup_opened');
+}
+
+// добавляем обработчик, не забыв про отключение дефолта
+// по сабмиту возвращаем значения, введенные в форме, приведя их к строкам 
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    console.log(profileNameInput.value);
+    profileName.textContent = profileNameInput.value;
+    console.log(profileJobInput.value);
+    profileJob.textContent = profileJobInput.value;
+    popupClose();
+}
+
+// добавляем вызов функций через подписку на события click и submit  
+
+btnEditProfile.addEventListener('click', popupOpen);
+btnClosePopup.addEventListener('click', popupClose);
+profileEditForm.addEventListener('submit', formSubmitHandler);
