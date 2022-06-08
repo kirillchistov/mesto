c2db1f69574c18e4879c01f7142d33f67c8748ab
Kirill Chistov <kirill.v.chistov@yandex.ru>
2022-06-08 22:50:07 +0300

    PR5 Profile Class naming, JS edits

--- js/index.js
+++ js/index.js
@@ -1,41 +1,42 @@
 // Находим в DOM попап и его элементы, присваиваем константам
 
-let popup = document.querySelector('.popup');
-let btnEditProfile = document.querySelector('.profile__button-edit');
-let btnClosePopup = document.querySelector('.popup__close');
-let profileEditForm = document.querySelector('.popup__form')
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
-let profileNameInput = document.querySelector('.popup__input_user_name');
-let profileJobInput = document.querySelector('.popup__input_user_job');
+const popup = document.querySelector('.popup');
+const popupProfile = document.querySelector('#popupProfile');
+const btnEditProfile = document.querySelector('.profile__button-edit');
+const btnClosePopup = document.querySelector('.popup__close');
+const profileEditForm = document.querySelector('.popup__form')
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+const profileNameInput = document.querySelector('.popup__input_user_name');
+const profileJobInput = document.querySelector('.popup__input_user_job');
 
 // добавляем модификатор, чтобы попап открывался 
 // Подставляем в поля текущие значения со страницы, убирая пробелы 
 
-function popupOpen() {
-    popup.classList.add('popup_opened');
+function popupProfileOpen() {
+    popupProfile.classList.add('popup_opened');
     profileNameInput.value = profileName.textContent.trim();
     profileJobInput.value = profileJob.textContent.trim();
 }
 
 // чтобы закрыть попап, удаляем модификатор 
 
-function popupClose() {
-    popup.classList.remove('popup_opened');
+function popupProfileClose() {
+    popupProfile.classList.remove('popup_opened');
 }
 
 // добавляем обработчик, не забыв про отключение дефолта
 // по сабмиту возвращаем значения, введенные в форме, приведя их к строкам 
 
-function formSubmitHandler (evt) {
+function formProfileSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = profileNameInput.value;
     profileJob.textContent = profileJobInput.value;
-    popupClose();
+    popupProfileClose();
 }
 
 // добавляем вызов функций через подписку на события click и submit  
 
-btnEditProfile.addEventListener('click', popupOpen);
-btnClosePopup.addEventListener('click', popupClose);
-profileEditForm.addEventListener('submit', formSubmitHandler);
+btnEditProfile.addEventListener('click', popupProfileOpen);
+btnClosePopup.addEventListener('click', popupProfileClose);
+profileEditForm.addEventListener('submit', formProfileSubmitHandler);
