65b733d32399d8ff6204496c6474c4d070cb4de2
Kirill Chistov <kirill.v.chistov@yandex.ru>
2022-06-17 17:59:51 +0300

    PR6 release 0.2 validation not working(

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Проект: Место
-## ПР №4,5 в Я.Практикуме. К. Чистов
+## ПР №4,5,6 в Я.Практикуме. К. Чистов
 
 ### Обзор
 * Интро
@@ -13,7 +13,7 @@
 
 
 **Интро**
-* Проект Место на [GitPages](https://kirillchistov.github.io/mesto/index.html).
+* Проект Место на [GHPages](https://kirillchistov.github.io/mesto/index.html).
 
 
 **Стек (Что будем использовать)**
@@ -23,32 +23,44 @@
 **Макет**
 * [Figma для ПР4](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
 * [Figma для ПР5](https://www.figma.com/file/bjyvbKKJN2naO0ucURl2Z0/JavaScript.-Sprint-5?node-id=0%3A1)
+* [Figma для ПР6](https://www.figma.com/file/kRVLKwYG3d1HGLvh7JFWRT/JavaScript.-Sprint-6?node-id=0%3A1)
 
 **Картинки**
 * Взяты из маркета в Фигме и UnSplash.
 * [Оптимизируем через TinyPNG](https://tinypng.com/), чтобы ускорить загрузку сайта.
 
-**Верстка**
-* Файловая структура по БЭМ.
+**Верстка ПР4**
+* Файловая структура по БЭМ. 
 * Вёрстка адаптивная: ширина зоны = f(ширины окна браузера). Не допускать горизонтальную полосу прокрутки. 
 * Минимальная ширина: 320px (одна карточка в ряд). Максимальная: 1280px (три карточки в ряд).
-* Есть 6 карточек «из коробки» Остальное из [Unsplash](https://unsplash.com/collections/8749236/russia).
 * Поп-ап редактирования профиля - поправить размер кнопки закрытия. - done
+* Размер текстовых полей в профиле в мобильной версии поправить - done
+* [Оттипографить текст](https://www.artlebedev.ru/typograf/)
+* [Чек-лист ПР4](https://code.s3.yandex.net/web-developer/checklists-pdf/new-program/checklist-4.pdf)
+
+**Разработка ПР5**
+* Есть 6 карточек «из коробки» и [Unsplash](https://unsplash.com/collections/8749236/russia).
+* Сделать рендеринг исходных карточек из JS-объекта - done 
 * Форма добавления карточки [Взять в Figma](https://www.figma.com/file/bjyvbKKJN2naO0ucURl2Z0/JavaScript.-Sprint-5?node-id=0%3A1) - done
 * Добавление карточки (имя и ссылка картинки вводятся пользователем) - done
 * Лайк карточки (сердечко меняет цвет) - done
 * Урна и Удаление карточки (как в макете) - done
 * Открытие попапа с картинкой и закрытие по кнопке - done
 * Плавное открытие и закрытие попапов - done
-* [Оттипографить текст](https://www.artlebedev.ru/typograf/)
-* [Подробнее](https://practicum.yandex.ru/learn/web/courses/35d951a1-b62c-4a96-96ac-a8118657fad0/sprints/16600/topics/d60394db-0f4a-4c6e-bede-9bb46bf7d968/lessons/60e3a5ca-91ae-4c7c-bb78-1d25a001e9d6/)
+* [Чек-лист ПР5](https://code.s3.yandex.net/web-developer/checklists-pdf/new-program/checklist-5.pdf)
+
+**Разработка ПР6**
+* Валидация формы «Редактировать профиль» [см. макет в Figma](https://www.figma.com/file/kRVLKwYG3d1HGLvh7JFWRT/JavaScript.-Sprint-6?node-id=0%3A1)
+* Валидация формы «Новое место» 
+* Закрытие попапа кликом на оверлей - done
+* Закрытие попапа нажатием на Esc - done
+* Учесть [требования к коду валидации форм](https://practicum.yandex.ru/learn/web/courses/35d951a1-b62c-4a96-96ac-a8118657fad0/sprints/34081/topics/43fd3acc-ab09-42b0-9a1a-478423a2650a/lessons/e42c1359-1e14-4586-bb92-ad1b44c6e0c4/) 
+* [Чек-лист ПР6](https://code.s3.yandex.net/web-developer/checklists-pdf/new-program/checklist-6.pdf)
 
 **Чек-лист**
-* [Чек-лист ПР4](https://code.s3.yandex.net/web-developer/checklists-pdf/new-program/checklist-4.pdf)
-* [Чек-лист ПР5](https://code.s3.yandex.net/web-developer/checklists-pdf/new-program/checklist-5.pdf)
-* Отформатировать код [в VSCode](https://codengineering.ru/q/how-do-you-format-code-in-visual-studio-code-vscode-27090) или [онлайн-сервисом](https://webformatter.com/). См. [стайл-гайд](https://code.s3.yandex.net/frontend-developer/landings/layout-design-rules/index.html)
+* Отформатировать код [в VSCode](https://codengineering.ru/q/how-do-you-format-code-in-visual-studio-code-vscode-27090) или [онлайн-сервисом](https://webformatter.com/). См. [styleguide](https://code.s3.yandex.net/frontend-developer/landings/layout-design-rules/index.html)
 
 **Доработки**
-* Доделать кнопку закрытия по комментариям ревьюера в ПР4 - done
-* Освоить стрелочные функции - done
-* Освоить forEach (на обработке событий попапов, кнопок) - done
+* Переписать функции на стрелочные - done
+* Разделить функции (1 функция = 1 фича) - TBD 
+* Починить вертикальный скролл (пропал в ПР5) - done
--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-/** Кнопки в профиле */
+// Кнопки в профиле //
 const btnEdit = document.querySelector('.profile__button-edit');
 const btnAdd = document.querySelector('.profile__button-add');
 
-/** Попап редактирования профиля */
+//   Попап редактирования профиля //
 const popupEdit = document.querySelector('#popupProfile');
 const formProfile = document.querySelector('#formProfileEdit');
 const nameInput = document.querySelector('.popup__input_user_name');
@@ -10,7 +10,7 @@ const jobInput = document.querySelector('.popup__input_user_job');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 
-/** Попап добавления нового места */
+//  Попап добавления нового места //
 const popupAdd = document.querySelector('#popupAddPlace');
 const gallery = document.querySelector('.elements');
 const elementTemplate = document.querySelector('#elementTemplate').content;
@@ -19,33 +19,109 @@ const placeNameInput = document.querySelector('.popup__input_place_name');
 const placeLinkInput = document.querySelector('.popup__input_place_link');
 const btnCreate = document.querySelector('#btnPopupAddPlaceSubmit');
 
-/** Попап открытия карточки места */
+//  Попап открытия карточки места //
 const popupElement = document.querySelector('#popupElement');
 const popupImage = popupElement.querySelector('.popup__image');
 const popupImageCaption = popupElement.querySelector('.popup__image-caption');
 const imgElements = document.querySelectorAll('.element__image');
 
-/** Кнопка закрытия попапа */
+// Все попапы в документе //
+const popupList = Array.from(document.querySelectorAll('.popup'));
+
+//  Кнопка закрытия попапа //
 const btnsClose = document.querySelectorAll('.popup__close');
 
-/** Открытие попаов */
-function openPopup(popup) {
+
+
+//  Открытие попаов //
+const openPopup = (popup) => {
   popup.classList.add('popup_opened');
+  document.addEventListener('keydown', (evt) => closeByEsc(evt, popup));
 };
 
-function editProfile(popup) {
+const editProfile = (popup) => {
   openPopup(popup);
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 };
 
-/** Закрытие попапов - удаление класса */
-function closePopup(popup) {
+//  Закрытие попапов - удаление класса и отписка //
+const closePopup = (popup) => {
   popup.classList.remove('popup_opened');
+  document.removeEventListener('keydown', (evt) => closeByEsc(evt, popup));
 };
 
-/** Создание карточки: клон элемента из шаблона, добавление названия и ссылки */
-function createCard(card) {
+
+/* Закрытие через Esc */
+const closeByEsc = (evt, popup) => {
+  if (evt.key === 'Escape') {
+      closePopup(popup)
+  }
+}
+
+// Закрытие нажатием на overlay
+const closeByOverlay = (popup) => {
+  popup.addEventListener('click', (evt) => {
+      if (evt.target === evt.currentTarget) {
+          closePopup(popup);
+      }
+  });
+};
+
+popupList.forEach((currentPopup) => {
+  closeByOverlay(currentPopup);
+});
+
+//  Форма добавления места: массив с названиями и ссылками из полей формы //
+const createCardForm = (evt) => {
+  evt.preventDefault();
+  const card = {};
+  card.link = placeLinkInput.value;
+  card.name = placeNameInput.value;
+  gallery.prepend(createCard(card));
+  closePopup(popupAdd);
+  formCreateCard.reset();
+};
+
+//  Обработка события сохранения изменений в профиле с отменой перезагрузки //
+const submitProfileFormHandler = (evt) => {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup(popupEdit);
+};
+
+//  Закрытие попапов - обработка клика по кнопке "x" ближайшего попапа //
+const handleClickClosePopup = (evt) => {
+  closePopup(evt.target.closest('.popup'));
+};
+
+const openImagePopup = (image) => {
+  image.querySelector('.element__image').addEventListener('click', evt => {
+    popupImage.src = evt.target.src;
+    popupImage.alt = evt.target.alt;
+    popupImage.title = evt.target.alt;
+    popupImageCaption.textContent = evt.target.alt;
+    openPopup(popupElement);
+  });
+};
+
+//  Удаление карточки места //
+const deleteItem = (item) => {
+  item.querySelector('.element__button-delete').addEventListener('click', evt => {
+    evt.target.closest('.element').remove();
+  });
+};
+
+//  Изменение кнопки Like  при клике //
+const toggleLike = (item) => {
+  item.querySelector('.element__button-like').addEventListener('click', evt => {
+    evt.target.classList.toggle('element__button-like_active');
+  })
+};
+
+//  Создание карточки: клон элемента из шаблона, добавление названия и ссылки //
+const createCard = (card) => {
   const galleryItem = elementTemplate.querySelector('.element').cloneNode(true);
   const elementItem = galleryItem.querySelector('.element__image');
   galleryItem.querySelector('.element__title').textContent = card.name;
@@ -57,68 +133,21 @@ function createCard(card) {
   return galleryItem;
 };
 
-/** Создание формы добавления карточки: создаем массив и заполняем свойства названия и ссылки из полей формы */
-function createCardForm(evt) {
-  evt.preventDefault();
-  const card = {};
-  card.link = placeLinkInput.value;
-  card.name = placeNameInput.value;
-  gallery.prepend(createCard(card));
-  closePopup(popupAdd);
-  formCreateCard.reset();
-};
 
 initialCards.forEach((card) => {
   const cardItem = createCard(card);
   gallery.prepend(cardItem);
 });
 
-/** Обработка события сохранения изменений в профиле с отменой перезагрузки */
-function submitProfileFormHandler (evt) {
-  evt.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
-  closePopup(popupEdit);
-};
-
-/** Закрытие попапов - обработка клика по кнопке "x" ближайшего попапа */
-function handleClickClosePopup(evt) {
-  closePopup(evt.target.closest('.popup'));
-};
-
-function openImagePopup(image) {
-  image.querySelector('.element__image').addEventListener('click', evt => {
-    popupImage.src = evt.target.src;
-    popupImage.alt = evt.target.alt;
-    popupImage.title = evt.target.alt;
-    popupImageCaption.textContent = evt.target.alt;
-    openPopup(popupElement);
-  });
-};
-
-/** Удаление карточки места */
-function deleteItem(item) {
-  item.querySelector('.element__button-delete').addEventListener('click', evt => {
-    evt.target.closest('.element').remove();
-  });
-};
-
-/** Стрелочная функция для изменения кнопки Like  при клике */
-function toggleLike(item) {
-  item.querySelector('.element__button-like').addEventListener('click', evt => {
-    evt.target.classList.toggle('element__button-like_active');
-  })
-};
-
-/** Подписка на события отправки форм профиля и Нового места */
+//  Подписка на события отправки форм профиля и Нового места //
 formProfile.addEventListener('submit', submitProfileFormHandler);
 formCreateCard.addEventListener('submit', createCardForm);
 
-/** Подписка на события кликов по кнопкам в профиле: */
+//  Подписка на события кликов по кнопкам в профиле: //
 btnEdit.addEventListener('click', () => editProfile(popupEdit));
 btnAdd.addEventListener('click', () => openPopup(popupAdd));
 
-/** Подписка на событие клика по кнопке "x" */
+//  Подписка на событие клика по кнопке "x" //
 btnsClose.forEach(button => {
   button.addEventListener('click', handleClickClosePopup);
 });
